f45e6baecd6a514ce9353839ec036f782de3ba31
xuyunfeng <2474286248@qq.com>
2023-06-05 13:56:52 +0800

    pinia 集成

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import router from '@/router'
 import App from './App.vue'
+import store from './store'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
